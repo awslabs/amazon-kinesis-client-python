a6c36ce25abd6d7426622913f2cf58e20037187e
Avinash Ravilla <ravillaa@amazon.com>
2021-06-04 11:19:01 -0700

    updated package version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ else:
 
 PACKAGE_NAME = 'amazon_kclpy'
 JAR_DIRECTORY = os.path.join(PACKAGE_NAME, 'jars')
-PACKAGE_VERSION = '2.0.1'
+PACKAGE_VERSION = '2.0.2'
 PYTHON_REQUIREMENTS = [
     'boto',
     # argparse is part of python2.7 but must be declared for python2.6
