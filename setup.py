2642ae73edd72cb2c0c70ca448294577a3702995
Yu Zeng <yuzen@amazon.com>
2021-12-10 11:56:20 -0800

    Import from pom instead

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ import sys
 
 import os
 import shutil
+import xml.etree.ElementTree as ET
+
 from setuptools import Command
 from setuptools import setup
 from setuptools.command.install import install
@@ -46,79 +48,11 @@ PYTHON_REQUIREMENTS = [
     # argparse is part of python2.7 but must be declared for python2.6
     'argparse',
 ]
-REMOTE_MAVEN_PACKAGES = [
-    # (group id, artifact id, version),
-    ('software.amazon.awssdk', 'kinesis', '2.17.52'),
-    ('software.amazon.awssdk', 'annotations', '2.17.52'),
-    ('software.amazon.awssdk', 'apache-client', '2.17.52'),
-    ('software.amazon.awssdk', 'auth', '2.17.52'),
-    ('software.amazon.awssdk', 'aws-cbor-protocol', '2.17.52'),
-    ('software.amazon.awssdk', 'aws-core', '2.17.52'),
-    ('software.amazon.awssdk', 'aws-json-protocol', '2.17.52'),
-    ('software.amazon.awssdk', 'aws-query-protocol', '2.17.52'),
-    ('software.amazon.awssdk', 'cloudwatch', '2.17.52'),
-    ('software.amazon.awssdk', 'dynamodb', '2.17.52'),
-    ('software.amazon.awssdk', 'http-client-spi', '2.17.52'),
-    ('software.amazon.awssdk', 'json-utils', '2.17.52'),
-    ('software.amazon.awssdk', 'metrics-spi', '2.17.52'),
-    ('software.amazon.awssdk', 'netty-nio-client', '2.17.52'),
-    ('software.amazon.awssdk', 'protocol-core', '2.17.52'),
-    ('software.amazon.awssdk', 'profiles', '2.17.52'),
-    ('software.amazon.awssdk', 'regions', '2.17.52'),
-    ('software.amazon.awssdk', 'sdk-core', '2.17.52'),
-    ('software.amazon.awssdk', 'sts', '2.17.52'),
-    ('software.amazon.awssdk', 'third-party-jackson-core', '2.17.52'),
-    ('software.amazon.awssdk', 'third-party-jackson-dataformat-cbor', '2.17.52'),
-    ('software.amazon.awssdk', 'utils', '2.17.52'),
-    ('software.amazon.eventstream', 'eventstream', '1.0.1'),
-    ('software.amazon.ion', 'ion-java', '1.5.1'),
-    ('software.amazon.glue', 'schema-registry-serde', '1.1.5'),
-    ('software.amazon.kinesis', 'amazon-kinesis-client-multilang', '2.3.9'),
-    ('software.amazon.kinesis', 'amazon-kinesis-client', '2.3.9'),
-    ('com.amazonaws', 'aws-java-sdk-core', '1.12.3'),
-    ('com.amazonaws', 'aws-java-sdk-sts', '1.12.3'),
-    ('com.amazonaws', 'jmespath-java', '1.12.3'),
-    ('com.fasterxml.jackson.core', 'jackson-annotations', '2.12.4'),
-    ('com.fasterxml.jackson.core', 'jackson-core', '2.12.4'),
-    ('com.fasterxml.jackson.core', 'jackson-databind', '2.12.4'),
-    ('com.fasterxml.jackson.dataformat', 'jackson-dataformat-cbor', '2.12.4'),
-    ('io.netty', 'netty-codec-http', '4.1.68.Final'),
-    ('io.netty', 'netty-codec-http2', '4.1.68.Final'),
-    ('io.netty', 'netty-codec', '4.1.68.Final'),
-    ('io.netty', 'netty-transport', '4.1.68.Final'),
-    ('io.netty', 'netty-resolver', '4.1.68.Final'),
-    ('io.netty', 'netty-common', '4.1.68.Final'),
-    ('io.netty', 'netty-buffer', '4.1.68.Final'),
-    ('io.netty', 'netty-handler', '4.1.68.Final'),
-    ('io.netty', 'netty-transport-native-epoll', '4.1.68.Final'),
-    ('io.netty', 'netty-transport-native-unix-common', '4.1.68.Final'),
-    ('io.reactivex.rxjava2', 'rxjava', '2.2.21'),
-    ('com.beust', 'jcommander', '1.81'),
-    ('com.google.guava', 'guava', '31.0.1-jre'),
-    ('com.google.code.findbugs', 'jsr305', '3.0.2'),
-    ('com.google.errorprone', 'error_prone_annotations', '2.7.1'),
-    ('com.google.j2objc', 'j2objc-annotations', '1.3'),
-    ('com.google.protobuf', 'protobuf-java', '3.17.1'),
-    ('com.typesafe.netty', 'netty-reactive-streams-http', '2.0.5'),
-    ('com.typesafe.netty', 'netty-reactive-streams', '2.0.5'),
-    ('org.apache.commons', 'commons-lang3', '3.12.0'),
-    ('org.apache.commons', 'commons-collections4', '4.4'),
-    ('org.apache.httpcomponents', 'httpclient', '4.5.13'),
-    ('org.apache.httpcomponents', 'httpcore', '4.4.14'),
-    ('org.checkerframework', 'checker-qual', '2.5.2'),
-    ('org.codehaus.mojo', 'animal-sniffer-annotations', '1.20'),
-    ('org.reactivestreams', 'reactive-streams', '1.0.3'),
-    ('org.slf4j', 'slf4j-api', '1.7.30'),
-    ('commons-beanutils', 'commons-beanutils', '1.9.4'),
-    ('commons-codec', 'commons-codec', '1.15'),
-    ('commons-collections', 'commons-collections', '3.2.2'),
-    ('commons-io', 'commons-io', '2.9.0'),
-    ('commons-logging', 'commons-logging', '1.2'),
-    ('ch.qos.logback', 'logback-classic', '1.2.7'),
-    ('ch.qos.logback', 'logback-core', '1.2.7'),
-    ('joda-time', 'joda-time', '2.10.10')
-]
 
+maven_pom_root = ET.parse('pom.xml').getroot()
+maven_pom_version = '{http://maven.apache.org/POM/4.0.0}'
+REMOTE_MAVEN_PACKAGES = [tuple(dep.find(maven_pom_version + attr).text for attr in ['groupId', 'artifactId', 'version'])
+                         for dep in maven_pom_root.iter(f'{maven_pom_version}dependency')]
 
 class MavenJarDownloader:
 
