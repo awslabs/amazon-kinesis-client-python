39d19023149b74119027835c5037aefe04c23c52
Thomas Trankle <60164225+ttrankle@users.noreply.github.com>
2025-04-22 16:09:48 -0400

    add ability to set mvn repo with env var (#274)
    Co-authored-by: rrz516 <thomas.trankle@capitalone.com>

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ Which will download the required jars and rerun the install.
         # Sample url:
         # https://search.maven.org/remotecontent?filepath=org/apache/httpcomponents/httpclient/4.2/httpclient-4.2.jar
         #
-        prefix = 'https://search.maven.org/remotecontent?filepath='
+        prefix = os.getenv("KCL_MVN_REPO_SEARCH_URL", 'https://search.maven.org/remotecontent?filepath=')
         return '{prefix}{path}/{artifact_id}/{version}/{dest}'.format(
                                         prefix=prefix,
                                         path='/'.join(group_id.split('.')),
