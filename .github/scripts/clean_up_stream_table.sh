f4945c6f434be9c89783c2a52cdee341c3d60779
skye rogers <skyero@amazon.com>
2025-07-03 11:09:48 -0700

    update is_fork and remove unused code

--- .github/scripts/clean_up_stream_table.sh
+++ .github/scripts/clean_up_stream_table.sh
@@ -15,15 +15,3 @@ aws dynamodb update-item \
     ":owner": {"S": "AVAILABLE"}
   }' \
   --return-values NONE
-
-# Delete all tables
-#for i in {1..5}; do
-#  aws dynamodb delete-table --table-name $APP_NAME && break ||
-#  echo "Retrying DynamoDB Table deletion in 10s" && sleep 10
-#done
-#for SUFFIX in "-CoordinatorState" "-WorkerMetricStats" "-LeaseManagement"; do
-#  if aws dynamodb describe-table --table-name $APP_NAME$SUFFIX &>/dev/null; then
-#    echo "Deleting table $APP_NAME$SUFFIX"
-#    aws dynamodb delete-table --table-name $APP_NAME$SUFFIX || true
-#  fi
-#done
